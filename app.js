694a13fec7fdbe8ad5838dd902d0c8bbd1c5daf4
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2022-08-06 19:50:18 +0530

    4th Auth

---
+++ app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const ej = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+ 
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+
+const User = new mongoose.model("User", userSchema);
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+  });
+
+
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post("/register", function(req, res) {
+
+   const salt = bcrypt.genSaltSync(saltRounds);
+   const hash = bcrypt.hashSync(req.body.password, salt);
+
+   const newUser = new User({
+    email: req.body.username,
+    password: hash
+   });
+
+   
+
+   newUser.save(function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        res.render("secrets");
+    }
+   });
+
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser) {
+    if(err) {
+        console.log(err);
+    } else {
+        if(foundUser) {
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true) {
+                    res.render("secrets");
+                }
+            });
+        }
+    }
+  });
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
